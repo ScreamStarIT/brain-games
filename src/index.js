3fbb291358e9ba291ec303cbf51caeb7aaf80386
ScreamStarIT <customsmith@gmail.com>
2021-01-27 22:39:09 +0700

    add index.js & made launch logic

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import readlineSync from 'readline-sync';
+import { userName } from './cli.js';
+
+const gameEngine = (manual, coreOfGame) => {
+  console.log(manual);
+  const winScore = 3;
+  for (let i = 0; i !== winScore;) {
+    const { task, rightAnswer } = coreOfGame();
+    console.log(`Question: ${task}`);
+    const answer = readlineSync.question('Your answer: ');
+    if (rightAnswer === answer) {
+      console.log('Correct!');
+      i += 1;
+    } else {
+      console.log(`"${answer}" is wrong answer ;(. Correct answer was "${rightAnswer}".`);
+      return console.log(`Let's try again, ${userName}!`);
+    }
+  }
+  return console.log(`Congratulations, ${userName}!`);
+};
+
+export default gameEngine;
